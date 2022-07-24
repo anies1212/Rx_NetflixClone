ed0626de5fe7611ba57394528d51d870d6d28229
anies1212 <all.for.me1212sna@gmail.com>
2022-07-24 17:09:58 +0900

    Initial Commit

---
+++ Rx_NetflixCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rx_NetflixCloneApp
+//
+//  Created by anies1212 on 2022/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
